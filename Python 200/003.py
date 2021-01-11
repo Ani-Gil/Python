3328e5dbec2ab57e08fc935f7ea82311ee9774f7
Ani-Gil <dlatldn426@gmail.com>
2021-01-11 10:38:06 +0900

    2021/01/11/10:38 Update

---
+++ Python 200/003.py
@@ -0,0 +1,7 @@
+# 003.py
+
+_myname = 'samsjang'
+my_name = '홍길동'
+MyName2 = 'Hong gil-dong'
+counter = 1
+Counter = 2
---
+++ Python 200/004.py
@@ -0,0 +1,7 @@
+# 004.py
+
+number1 = 1
+pi = 3.14
+flag = True
+char = 'x'
+chars = 'I love Python'
