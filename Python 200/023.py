3aedae4f91345286a880e0158dcddff7896f4199
Ani-Gil <dlatldn426@gmail.com>
2021-01-15 14:10:05 +0900

    2021/01/15/15:10 Update

---
+++ Python 200/023.py
@@ -0,0 +1,9 @@
+# 023.py
+
+x = 1; y = 2
+str1 = 'abc'; str2 = 'python'
+print(x == y)               # False가 출력됨
+print(str1 == str2)         # True가 출력됨
+print(str2 == 'python')     # False가 출력됨
+print(str2 == 'python')     # True가 출력됨
+print(str1 < str2)          # True가 출력됨
