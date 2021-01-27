948bec8342b3630db5bc2ef509d3b0caac5475d8
Ani-Gil <dlatldn426@gmail.com>
2021-01-27 12:40:04 +0900

    2021/01/27/12:40 Update

---
+++ Python 200/069.py
@@ -0,0 +1,8 @@
+# 069.py - 실수형 자료를 정수형 자료로 변환하기(int)
+
+idata1 = int(-5.4)
+idata2 = int(1.78e1)
+idata3 = int(171.56)
+print(idata1)       # -5가 출력됨
+print(idata2)       # 17이 출력됨
+print(idata3)       # 171이 출력됨
