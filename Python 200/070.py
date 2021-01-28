8b5c0f0cc75fa66d237242905196b65694f7f4c7
Ani-Gil <dlatldn426@gmail.com>
2021-01-28 15:02:50 +0900

    2021/01/28/15:02 Update

---
+++ Python 200/070.py
@@ -0,0 +1,4 @@
+# 070.py - 정수형 자료를 실수형 자료로 변환하기(float)
+
+fdata = float(10)
+print(fdata)    # 10.0으로 출력됨
---
+++ Python 200/072.py
@@ -0,0 +1,18 @@
+# 072.py - 최대값, 최소값 구하기(max, min)
+
+listdata = [9.96, 1.27, 5.07, 6.45, 8.38, 9.29, 4.93, 7.73, 3.71, 0.93]
+maxval = max(listdata)
+minval = min(listdata)
+print(maxval)       # 9.96이 출력됨
+print(minval)       # 0.93이 출력됨
+
+txt = 'Alotofthingsoccureachday'
+maxval = max(txt)
+minval = min(txt)
+print(maxval)       # 'y'가 출력됨
+print(minval)       # 'A'가 출력됨
+
+maxval = max(2+3, 2*3, 2**3, 3**2)
+minval = min('abz', 'al2')
+print(maxval)       # 9가 출력됨
+print(minval)       # 'al2'가 출력됨
