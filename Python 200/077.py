4c0f863033d7d84eac12fc8420b11c5ef3596f76
Ani-Gil <dlatldn426@gmail.com>
2021-01-29 19:39:03 +0900

    2021/01/29/19:39 Update

---
+++ Python 200/077.py
@@ -0,0 +1,5 @@
+# 077.py - 문자열에서 홀수 번째 문자만 추출하기
+
+txt = 'aAbBcCdDeEfFgGhHiIjJkK'
+ret = txt[::2]
+print(ret)      # 'abcdefghijk' 가 출력됨
---
+++ Python 200/078.py
@@ -0,0 +1,5 @@
+# 078.py - 문자열을 거꾸로 만들기
+
+txt = 'abcdefghijk'
+ret = txt[::-1]
+print(ret)      # 'kjihgfedcba' 가 출력됨
